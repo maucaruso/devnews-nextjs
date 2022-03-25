994647ec59fa2dbe147f7983cf8f62ac2d88befb
maurici3 <carusojr@outlook.com>
2022-03-25 14:05:22 -0300

    chore: settings prismic and styles for posts page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,25 @@
-import { GetServerSideProps } from 'next';
 import SEO from '../components/SEO';
-//import { useEffect, useState } from 'react';
+import styles from '../styles/home.module.scss';
 
-interface Post {
-  id: string;
-  title: string;
-}
-
-interface HomeProps {
-  posts: Post[];
-}
-
-export default function Home({ posts }: HomeProps) {
-  // const [posts, setPosts] = useState<Post[]>([]);
-
-  // useEffect(() => {
-  //   fetch('http://localhost:3333/posts').then(response => {
-  //     response.json().then(data => {
-  //       setPosts(data);
-  //     });
-  //   });
-  // }, []);
-
+export default function Home() {
   return (
-    <div>
-      <SEO title="Home" />
-      <h1>Posts</h1>
-      <ul>
-        {posts.map(post => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <SEO title="Dev News" excludeTitleSuffix />
+
+      <main className={styles.content}>
+        <section className={styles.section}>
+          <span>Olá Dev!</span>
+          <h1>
+            Bem-vindo ao <span>Dev</span>News!
+          </h1>
+          <p>
+            Um blog com conteúdos extremamente <br /> <span>relavantes</span>{' '}
+            para seu aprendizado.
+          </p>
+        </section>
+
+        <img src="/home.svg" alt="Home image" />
+      </main>
+    </>
   );
 }
-
-export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
-  const response = await fetch('http://localhost:3333/posts');
-  const posts = await response.json();
-
-  return {
-    props: {
-      posts,
-    },
-  };
-};
